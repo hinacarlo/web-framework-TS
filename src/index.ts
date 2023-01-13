48a5cf1fb714fdb9d25b3164c1f4ddd749193934
hinacarlo <hinacarlo1993@gmail.com>
2023-01-13 16:02:39 +0800

    Add on and trigger method on the user class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Carlo', age: 28 });
+user.on('click', () => console.log('click1'));
+user.on('change', () => console.log('change2'));
+user.on('click', () => console.log('click2'));
 
-user.set({ name: 'Gelo', age: 999 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('click');
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,14 +3,45 @@ interface UserData {
   age?: number;
 }
 
-export class User {
-  constructor(private data: UserData) {}
+type Callback = () => void;
 
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserData) {} // object to store information from parameter
+
+  // gets a single piece of info about this user
   get(propName: string): number | string {
     return this.data[propName];
   }
 
+  // changes info about this user (name, age)
   set(update: UserData): void {
-    Object.assign(this.data, update);
+    /* Object.assign(this.data, update); */
+    this.data = { ...this.data, ...update };
   }
+
+  // registers an event handler with this object so other parts of the app
+  // know when something changes
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  // triggers an event to tell other parts of the app that something has changed
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((cb) => {
+      cb();
+    });
+  }
+
+  // fetches some data from the server about a particular user
+  /* fetch(): Promise */
+
+  // saves some data about this user to the server
+  /* save(): Promise */
 }
