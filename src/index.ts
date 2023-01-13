0e8020e52244c8862142f4d11b76c0ba3db3ef19
hinacarlo <hinacarlo1993@gmail.com>
2023-01-13 12:45:49 +0800

    Add get and set method on the user class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi');
+import { User } from './models/User';
+
+const user = new User({ name: 'Carlo', age: 28 });
+
+user.set({ name: 'Gelo', age: 999 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
---
+++ src/models/User.ts
@@ -0,0 +1,16 @@
+interface UserData {
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  constructor(private data: UserData) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserData): void {
+    Object.assign(this.data, update);
+  }
+}
